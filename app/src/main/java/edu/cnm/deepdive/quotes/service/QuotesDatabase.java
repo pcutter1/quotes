67cd4189a2ef8dab834bbf277d78fae7146808f0
Paul C <pcutter@cnm.edu>
2020-06-17 12:01:28 -0600

    version 2 - added callback and completed quotes parser

--- app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
@@ -1,24 +1,34 @@
 package edu.cnm.deepdive.quotes.service;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
+import edu.cnm.deepdive.quotes.R;
 import edu.cnm.deepdive.quotes.model.dao.QuoteDao;
 import edu.cnm.deepdive.quotes.model.dao.SourceDao;
 import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
+import io.reactivex.internal.observers.QueueDrainObserver;
 import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 @Database(
     entities = {Source.class, Quote.class},
@@ -47,6 +57,7 @@ public abstract class QuotesDatabase extends RoomDatabase {
 
     private static final QuotesDatabase INSTANCE =
         Room.databaseBuilder(context, QuotesDatabase.class, DB_NAME)
+            .addCallback(new QuotesCallback())
              .build();
 
   }
@@ -56,20 +67,67 @@ public abstract class QuotesDatabase extends RoomDatabase {
     @Override
     public void onCreate(@NonNull SupportSQLiteDatabase db) {
       super.onCreate(db);
-      //TODO Create a map from file contents
-
-      //TODO Persist map contents to database.
+      try {
+        Map<Source, List<Quote>> map = parseFile(R.raw.quotes);
+        persist(map);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
     }
 
-    private Map<Source, List<Quote>> paraseFile(int resourceId) throws IOException {
+    private Map<Source, List<Quote>> parseFile(int resourceId) throws IOException {
       try (
           InputStream input = QuotesDatabase.context.getResources().openRawResource(resourceId);
           Reader reader = new InputStreamReader(input);
           CSVParser parser = CSVParser.parse(
               reader, CSVFormat.EXCEL.withIgnoreSurroundingSpaces().withIgnoreEmptyLines());
       ) {
-        // TODO Add records from parser to map.
+       Map<Source, List<Quote>> map = new HashMap<>();
+
+       for (CSVRecord record : parser) {
+         Source source = null;
+         String sourceName = record.get(0).trim();
+         if (!sourceName.isEmpty()) {
+           source = new Source();
+           source.setName(sourceName);
+         }
+         List<Quote> quotes = map.computeIfAbsent(source, (s) -> new LinkedList<>());
+         Quote quote = new Quote();
+         quote.setText(record.get(1).trim());
+         quotes.add(quote);
+       }
+       return map;
       }
+    }
+
+    @SuppressLint("CheckResult")
+    private void persist(Map<Source, List<Quote>> map) {
+      QuotesDatabase database = QuotesDatabase.getInstance();
+      SourceDao sourceDao = database.getSourceDao();
+      QuoteDao quoteDao = database.getQuoteDao();
+      List<Source> sources = new LinkedList<>(map.keySet());
+      List<Quote> unattributed = map.getOrDefault(null, Collections.emptyList());
+      sources.remove(null);
+      sourceDao.insert(sources)
+          .subscribeOn(Schedulers.io())
+          .flatMap((sourceIds) -> {
+            List<Quote> quotes  = new LinkedList<>();
+            Iterator<Long> idIterator = sourceIds.iterator();
+            Iterator<Source> sourceIterator = sources.iterator();
+            while (idIterator.hasNext()) {
+              long sourceId = idIterator.next();
+              for (Quote quote : map.getOrDefault(sourceIterator.next(), Collections.emptyList())) {
+                quote.setSourceId(sourceId);
+                quotes.add(quote);
+              }
+            }
+            quotes.addAll(unattributed);
+            return quoteDao.insert(quotes);
+          })
+          .subscribe(
+              (quoteIds) -> {},
+              (throwable) -> {throw new RuntimeException(throwable);}
+          );
     }
   }
 
